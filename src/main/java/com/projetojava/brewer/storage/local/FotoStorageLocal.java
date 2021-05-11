33fab148bf8651dfc3f15eb989def66f047c317a
brian-izaki <bkizaki@outlook.com>
2021-05-11 12:17:23 -0300

    salvando imagem com uuid e no diretorio temp

--- src/main/java/com/projetojava/brewer/storage/local/FotoStorageLocal.java
+++ src/main/java/com/projetojava/brewer/storage/local/FotoStorageLocal.java
@@ -7,9 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.UUID;
 
 public class FotoStorageLocal implements FotoStorage {
     private static final Logger logger = LoggerFactory.getLogger(FotoStorageLocal.class);
@@ -24,6 +26,24 @@ public class FotoStorageLocal implements FotoStorage {
     public FotoStorageLocal(Path path) {
         this.local = path;
         criarPastas();
+    }
+
+    @Override
+    public String salvarTemporariamente(MultipartFile[] files) {
+        String novoNome = null;
+        if (files != null && files.length > 0) {
+            MultipartFile arquivo = files[0];
+            novoNome = renomearArquivo(arquivo.getOriginalFilename());
+            try {
+                arquivo.transferTo(
+                        new File(this.localTemporario.toAbsolutePath().toString()
+                                + getDefault().getSeparator()
+                                + novoNome));
+            } catch (IOException e) {
+                throw new RuntimeException("Erro salvando a foto na pasta temporária");
+            }
+        }
+        return novoNome;
     }
 
     private void criarPastas() {
@@ -43,8 +63,8 @@ public class FotoStorageLocal implements FotoStorage {
         }
     }
 
-    @Override
-    public void salvarTemporariamente(MultipartFile[] files) {
-        System.out.println(">>>> salvando foto temporariamente");
+    private String renomearArquivo(String nomeOriginal){
+        String novoNome = UUID.randomUUID().toString() + "_" + nomeOriginal;
+        return novoNome;
     }
 }
