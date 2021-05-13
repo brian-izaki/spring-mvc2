c1c598a66692872c6756b0499e9a20366169cd31
brian-izaki <bkizaki@outlook.com>
2021-05-13 17:07:47 -0300

    add ordenação da listagem de estilos

--- src/main/java/com/projetojava/brewer/repository/helper/estilo/EstilosImpl.java
+++ src/main/java/com/projetojava/brewer/repository/helper/estilo/EstilosImpl.java
@@ -5,11 +5,13 @@ import com.projetojava.brewer.repository.filter.EstiloFilter;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -17,7 +19,7 @@ import javax.persistence.PersistenceContext;
 
 import java.util.List;
 
-public class EstilosImpl implements EstilosQueries{
+public class EstilosImpl implements EstilosQueries {
 
     @PersistenceContext
     private EntityManager manager;
@@ -36,6 +38,14 @@ public class EstilosImpl implements EstilosQueries{
         criteria.setMaxResults(totalRegistrosPorPagina);
 
         aplicarFiltro(filter, criteria);
+
+        Sort sort = pageable.getSort();
+
+        if (sort != null) {
+            Sort.Order order = sort.iterator().next();
+            String property = order.getProperty();
+            criteria.addOrder(order.isAscending() ? Order.asc(property) : Order.desc(property));
+        }
 
         return new PageImpl(criteria.list(), pageable, total(filter));
     }
