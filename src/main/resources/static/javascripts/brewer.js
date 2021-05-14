b75d5fba59933a3adf5ea440dfeb00c5c5508d0b
brian-izaki <bkizaki@outlook.com>
2021-05-14 09:52:00 -0300

    mascara para telefone

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -12,10 +12,34 @@ class MaskMoney {
   }
 }
 
+class MaskPhoneNumber {
+  constructor() {
+    this.inputPhoneNumber = $('.js-phone-number');
+  }
+
+  enable() {
+    const maskBehavior = function(val) {
+      return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009'
+    }
+
+    const options = {
+      oneKeyPress: function(val, e, field, options) {
+        field.mask(maskBehavior.apply({}, arguments), options);
+      }
+    }
+
+    this.inputPhoneNumber.mask(maskBehavior, options);
+  }
+
+}
+
 Brewer.MaskMoney = MaskMoney;
+Brewer.MaskPhoneNumber = MaskPhoneNumber;
 
 $(function() {
   const maskMoney = new Brewer.MaskMoney();
+  const maskPhoneNumber = new Brewer.MaskPhoneNumber();
 
   maskMoney.enable();
+  maskPhoneNumber.enable();
 })
