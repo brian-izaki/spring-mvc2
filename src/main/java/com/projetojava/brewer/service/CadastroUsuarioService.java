a4690399857c5d6c915c189be00b48291d7c2867
brian-izaki <bkizaki@outlook.com>
2021-05-19 11:22:52 -0300

    add Spring security e BCrypt ao salvar senhas

--- src/main/java/com/projetojava/brewer/service/CadastroUsuarioService.java
+++ src/main/java/com/projetojava/brewer/service/CadastroUsuarioService.java
@@ -5,6 +5,7 @@ import com.projetojava.brewer.repository.Usuarios;
 import com.projetojava.brewer.service.exception.EmailUsuarioJaExistenteException;
 import com.projetojava.brewer.service.exception.SenhaObrigatoriaUsuarioException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
@@ -17,6 +18,10 @@ public class CadastroUsuarioService {
     @Autowired
     private Usuarios usuarios;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
     @Transactional
     public void salvar(Usuario usuario) {
         Optional<Usuario> emailOptional = usuarios.findByEmailIgnoreCase(usuario.getEmail());
@@ -25,8 +30,12 @@ public class CadastroUsuarioService {
             throw new EmailUsuarioJaExistenteException("E-mail digitado já existe");
         }
 
-        if (usuario.isNovo() && StringUtils.isEmpty(usuario.getSenha())) {
-            throw new SenhaObrigatoriaUsuarioException("Senha é obrigatória para novo usuário");
+        if (usuario.isNovo()) {
+            if (StringUtils.isEmpty(usuario.getSenha()))
+                throw new SenhaObrigatoriaUsuarioException("Senha é obrigatória para novo usuário");
+
+            usuario.setSenha(this.passwordEncoder.encode(usuario.getSenha()));
+            usuario.setConfirmacaoSenha(usuario.getSenha());
         }
 
         usuarios.save(usuario);
