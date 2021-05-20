f98cf5279a30ad0ca5b10e14895427e36df43c1c
brian-izaki <bkizaki@outlook.com>
2021-05-20 15:26:53 -0300

    extendendo User do S. Security

--- src/main/java/com/projetojava/brewer/security/AppUserDetailsService.java
+++ src/main/java/com/projetojava/brewer/security/AppUserDetailsService.java
@@ -27,7 +27,7 @@ public class AppUserDetailsService implements UserDetailsService {
         // expressão lambda
         Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
 
-        return new User(usuario.getEmail(), usuario.getSenha(), getPermissoes(usuario));
+        return new UsuarioSistema(usuario, getPermissoes(usuario));
     }
 
     private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
