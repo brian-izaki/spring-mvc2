07a7c923b698a8fd87cbe80401bd8ff222ddc919
brian-izaki <bkizaki@outlook.com>
2021-05-25 14:46:33 -0300

    mapeando e criando tabela de venda e item_venda

--- src/main/java/com/projetojava/brewer/model/ItemVenda.java
+++ src/main/java/com/projetojava/brewer/model/ItemVenda.java
@@ -1,14 +1,32 @@
 package com.projetojava.brewer.model;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Objects;
 
-public class ItemVenda {
+@Entity
+@Table(name = "item_venda")
+public class ItemVenda implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long codigo;
+
     private Integer quantidade;
+
+    @Column(name = "valor_unitario")
     private BigDecimal valorUnitario;
+
+    @ManyToOne
+    @JoinColumn(name = "codigo_cerveja")
     private Cerveja cerveja;
+
+    @ManyToOne
+    @JoinColumn(name = "codigo_venda")
+    private Venda venda;
 
     public BigDecimal getValorTotal() {
         return valorUnitario.multiply(new BigDecimal(quantidade));
@@ -44,6 +62,14 @@ public class ItemVenda {
 
     public void setCerveja(Cerveja cerveja) {
         this.cerveja = cerveja;
+    }
+
+    public Venda getVenda() {
+        return venda;
+    }
+
+    public void setVenda(Venda venda) {
+        this.venda = venda;
     }
 
     @Override
---
+++ src/main/java/com/projetojava/brewer/model/Venda.java
@@ -0,0 +1,157 @@
+package com.projetojava.brewer.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "venda")
+public class Venda implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    @NotNull(message = "Data de Criação é obrigatória")
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+
+    @Column(name = "valor_frete")
+    private BigDecimal valorFrete;
+
+    @Column(name = "valor_desconto")
+    private BigDecimal valorDesconto;
+
+    @NotNull(message = "Valor Total é obrigatório")
+    @Column(name = "valor_total")
+    private BigDecimal valorTotal;
+
+    private String observacao;
+
+    @Column(name = "data_entrega")
+    private LocalDateTime dataEntrega;
+
+    @NotNull(message = "Status da venda é obrigatório")
+    @Enumerated(EnumType.STRING)
+    private StatusVenda status;
+
+    @NotNull(message = "O cliente é obrigatório")
+    @ManyToOne
+    @JoinColumn(name = "codigo_cliente")
+    private Cliente cliente;
+
+    @NotNull(message = "Usuário é obrigatório")
+    @ManyToOne
+    @JoinColumn(name = "codigo_usuario")
+    private Usuario usuario;
+
+    @OneToMany(mappedBy = "venda")
+    private List<ItemVenda> itens;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public BigDecimal getValorFrete() {
+        return valorFrete;
+    }
+
+    public void setValorFrete(BigDecimal valorFrete) {
+        this.valorFrete = valorFrete;
+    }
+
+    public BigDecimal getValorDesconto() {
+        return valorDesconto;
+    }
+
+    public void setValorDesconto(BigDecimal valorDesconto) {
+        this.valorDesconto = valorDesconto;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public StatusVenda getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusVenda status) {
+        this.status = status;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public LocalDateTime getDataEntrega() {
+        return dataEntrega;
+    }
+
+    public void setDataEntrega(LocalDateTime dataEntrega) {
+        this.dataEntrega = dataEntrega;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public List<ItemVenda> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemVenda> itens) {
+        this.itens = itens;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Venda venda = (Venda) o;
+        return Objects.equals(codigo, venda.codigo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo);
+    }
+}
